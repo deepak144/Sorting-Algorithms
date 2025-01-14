c1210ef168f8b6c1b85beb6a637c30fb8b289cd2
deepak berwal <76567665+deepak144@users.noreply.github.com>
2025-01-14 10:03:13 +0000

    implemented selection sort in bubble sort code

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,5 +1,29 @@
 #include "stdio.h"
 
+
+void selection_sort(int arr[],int size){
+
+    for (int i = 0; i < size ; i++){
+        int smallest_index = i;
+        for (int j = i+1; j < size ; j++){
+            if(arr[j]<arr[smallest_index]){
+                smallest_index = j;
+            }
+        }
+        //swap 
+        if(smallest_index != i){
+            int temp = arr[smallest_index];
+            arr[smallest_index] = arr[i];
+            arr[i] = temp;
+        }
+        
+
+    }
+
+}
+
+
+
 void bubble_sort(int arr[], int size){
     
     for (int i = 0; i < size; i++){
@@ -19,9 +43,16 @@ void bubble_sort(int arr[], int size){
 void main ()
 {
     int arr[8] = {51,32,64,15,96,7,8};
+    int arr2[8] = {51,32,64,15,96,7,8};
     int size = sizeof(arr)/sizeof(arr[0]);
     bubble_sort(arr,size);
+    printf("\n Bubble sort   : ");
     for (int i = 0; i<size; i++){
         printf("%d ",arr[i]);
+    }
+     selection_sort(arr2,size);
+     printf("\nSelection sort : ");
+     for (int i = 0; i<size; i++){
+        printf("%d ",arr2[i]);
     }
 }
